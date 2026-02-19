a11cfbe32176e3cd78a83b035aa9104e0e79eb95
Wellington Cardoso <wellingtoncardoso1514@gmail.com>
2026-02-18 22:04:07 -0300

    Adicionar arquivos de projeto.

---
+++ ExercicesC#1/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace ExercicesC_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(fibonacciGerar(5));
+            Console.WriteLine(gerarFatorial(5));
+        }
+
+        public static List<int> fibonacciGerar(int n)
+        {
+            List<int> lista = new List<int>();
+
+            if (n <= 0)
+            {
+                throw new Exception("Não pode ser menor ou igual a 0");
+            }
+
+            lista.Add(0);
+
+            if (n == 1)
+            {
+                return lista;
+            }
+
+            lista.Add(1);
+
+            for (int i = 2; i <= n; i++)
+            {
+                int proximo = lista[i - 1] + lista[i - 2];
+                lista.Add(proximo);
+            }
+
+            return lista;
+        }
+
+
+        public static int gerarFatorial(int n)
+        {
+            if (n <= 0)
+            {
+                throw new Exception("Não pode ser menor ou igual a 0");
+            }
+
+            int resultado = 1;
+
+            for (int i = n; i > 1; i--)
+            {
+                resultado = resultado * i;
+            }
+
+            return resultado;
+        }
+    }
+}
